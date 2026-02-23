d475364ad828b6f7de5e5d60ac77c1ab71543dc1
hyunahparc <hyunah.parc@gmail.com>
2026-02-24 00:40:29 +0100

    feat: add swagger docs and server entry point

---
+++ supcontent-backend/server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const passport = require('passport');
+const swaggerUi = require('swagger-ui-express');
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+require('./src/config/db');
+require('./src/config/passport');
+
+const app = express();
+
+app.use(express.json());
+app.use(passport.initialize());
+
+// Routes
+const authRouter = require('./src/routes/auth.route');
+
+app.use('/api/auth', authRouter);
+
+// Swagger
+const swaggerDocument = yaml.load(fs.readFileSync('./swagger.yaml', 'utf8'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server running on http://localhost:${process.env.PORT || 3000}`);
+});
+
+module.exports = { app, server };
