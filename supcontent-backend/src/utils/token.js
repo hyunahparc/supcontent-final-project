26b1a7d8a68418384cd5d8a3d3b84ec0c605d362
hyunahparc <hyunah.parc@gmail.com>
2026-02-24 15:56:16 +0100

    refactor: extract token generation into utility function

---
+++ supcontent-backend/src/utils/token.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+
+const generateToken = (user) => {
+    return jwt.sign(
+        { user_id: user.user_id, email: user.email },
+        process.env.JWT_SECRET,
+        { expiresIn: '7d' }
+    );
+};
+
+module.exports = generateToken;
